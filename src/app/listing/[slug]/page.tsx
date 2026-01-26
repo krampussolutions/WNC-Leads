3360245fb35ca432508dd82cfe4daced77b991b2
Matthew Poteet <krampussolutions@gmail.com>
2026-01-26 15:23:22 -0500

    Improve params debug for listing route

--- src/app/listing/[slug]/page.tsx
+++ src/app/listing/[slug]/page.tsx
@@ -1,10 +1,23 @@
 export const dynamic = "force-dynamic";
 
-export default function ListingPage(props: any) {
+export default function ListingPage({ params, searchParams }: any) {
+  const slug = params?.slug;
+
   return (
     <div style={{ color: "white", padding: 50 }}>
       <h1>PARAM DEBUG</h1>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+
+      <div>slug (direct): {String(slug)}</div>
+      <div>keys(params): {JSON.stringify(Object.keys(params ?? {}))}</div>
+      <div>ownProps(params): {JSON.stringify(Object.getOwnPropertyNames(params ?? {}))}</div>
+
+      <pre style={{ marginTop: 20 }}>
+        props.params (stringify): {JSON.stringify(params, null, 2)}
+      </pre>
+
+      <pre style={{ marginTop: 20 }}>
+        searchParams: {JSON.stringify(searchParams, null, 2)}
+      </pre>
     </div>
   );
 }
