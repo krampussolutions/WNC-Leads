53390e986b7c9dab1a2f687dc5c5ac35def7c9e0
Matthew Poteet <krampussolutions@gmail.com>
2026-01-26 15:25:29 -0500

    Fix listing params by awaiting Next.js params

--- src/app/listing/[slug]/page.tsx
+++ src/app/listing/[slug]/page.tsx
@@ -1,23 +1,26 @@
 export const dynamic = "force-dynamic";
 
-export default function ListingPage({ params, searchParams }: any) {
+type PageProps = {
+  params: Promise<{ slug: string }> | { slug: string };
+  searchParams: Promise<Record<string, any>> | Record<string, any>;
+};
+
+async function resolve<T>(v: T | Promise<T>): Promise<T> {
+  return v && typeof (v as any).then === "function" ? await (v as any) : (v as any);
+}
+
+export default async function ListingPage(props: PageProps) {
+  const params = await resolve(props.params);
+  const searchParams = await resolve(props.searchParams);
+
   const slug = params?.slug;
 
   return (
     <div style={{ color: "white", padding: 50 }}>
       <h1>PARAM DEBUG</h1>
-
-      <div>slug (direct): {String(slug)}</div>
-      <div>keys(params): {JSON.stringify(Object.keys(params ?? {}))}</div>
-      <div>ownProps(params): {JSON.stringify(Object.getOwnPropertyNames(params ?? {}))}</div>
-
-      <pre style={{ marginTop: 20 }}>
-        props.params (stringify): {JSON.stringify(params, null, 2)}
-      </pre>
-
-      <pre style={{ marginTop: 20 }}>
-        searchParams: {JSON.stringify(searchParams, null, 2)}
-      </pre>
+      <div>slug (awaited): {String(slug)}</div>
+      <pre style={{ marginTop: 20 }}>params: {JSON.stringify(params, null, 2)}</pre>
+      <pre style={{ marginTop: 20 }}>searchParams: {JSON.stringify(searchParams, null, 2)}</pre>
     </div>
   );
 }
